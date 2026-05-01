666e68a9ec70829d129a96deba4db00998c5f821
Archie Flude <139282112+MrGoogle24@users.noreply.github.com>
2026-05-01 12:02:00 +0100

    Added Hyrax Page

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,8 @@
-document.getElementById("loginForm").addEventListener("submit", function() {
+const form = document.getElementById("loginForm");
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); // stop page reload / POST
+
     alert("You reached the end of the website! This is where I've hit the end and no longer can be bothered to do any more. I'm really curious about how long we can make the alert box in javascript, it seems like I may be able to type into here forever because surely the box will just get bigger right? That seems to make sense to me but you never know maybe the internet is boring and useless and doesn't like people who want to have fun and write massive alerts into their code! Anyway I'm bored now so well done for reading to the end!");
 });
