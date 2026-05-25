851d4704c107b93b66b954bac629e45c2cc60520
Archie Flude <139282112+MrGoogle24@users.noreply.github.com>
2026-05-25 21:17:58 +0100

    starting to make the password work again!

--- login/login.js
+++ login/login.js
@@ -1,8 +1,9 @@
-const form = document.getElementById("loginForm");
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+   e.preventDefault();
 
+   const username = document.getElementById("username").value;
+   const password = document.getElementById("password").value;
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault(); // stop page reload / POST
-
-    alert("You reached the end of the website! This is where I've hit the end and no longer can be bothered to do any more. I'm really curious about how long we can make the alert box in javascript, it seems like I may be able to type into here forever because surely the box will just get bigger right? That seems to make sense to me but you never know maybe the internet is boring and useless and doesn't like people who want to have fun and write massive alerts into their code! Anyway I'm bored now so well done for reading to the end!");
+    console.log("Username:", username);
+    console.log("Password:", password);
 });
