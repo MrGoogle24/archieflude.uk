4f66811cab22c716aaf94456484f31d1d23dd82b
Archie Flude <139282112+MrGoogle24@users.noreply.github.com>
2026-07-31 21:58:05 +0100

    fixed join system

--- ellies_quiz/index.js
+++ ellies_quiz/index.js
@@ -1,11 +1,13 @@
 async function enterName() {
     const name = document.getElementById("name").value;
+    console.log(name)
     const res = await fetch("/join", {
         method: "POST",
         headers: {"content-type": "application/json"},
         body: JSON.stringify({ "name": name, "points": 0 }),
     });
-    console.log(await res.json());
+    console.log(res.text())
+    console.log(res.json())
 }
 
 window.enterName = enterName;
